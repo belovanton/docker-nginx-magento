8d7e5e9bf1c16b64fe2a6a03cbb2dec880f15463
dimasch <d.shegolihin@gmail.com>
2015-03-08 00:02:41 +0400

    Added select DB in first argument for seturl script.

--- scripts/seturl.php
+++ scripts/seturl.php
@@ -1,9 +1,9 @@
 <?php
 $httphost = 'local.kmplzt.de'; //($_SERVER['HTTP_HOST']);
 mysql_connect('db', 'root', '123') or die ('mysql error');
-mysql_select_db('magento');
+mysql_select_db($argv[1]);
 mysql_query('update core_config_data set value="http://'.$httphost.'/" where path like "%secure/base_url"') or die ("error query");
 
-echo "host set to: " . $httphost . "\n";
+echo "host set to: ". $httphost ." for db ". $args[1] ."\n";
 
 system("rm -Rf var/cache/mage*");
